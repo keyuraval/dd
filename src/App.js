120c4e110d5b1fe2e3554061880ef29ddf37f4a4
Keyur Raval <ravalkeyur054@gmail.com>
2022-11-23 00:13:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import CreatableSelect from "react-select/creatable";
 
-function App() {
+const App = () => {
+  const options = [
+    { value: "purple", label: "Purple", color: "#A020F0" },
+    { value: "red", label: "Red", color: "#FF0000" },
+    { value: "orange", label: "Orange", color: "#FFA500" },
+    { value: "green", label: "Green", color: "#00FF00" }
+
+  ];
+  const colorStyles = {
+    control: (styles) => ({ ...styles, backgroundColor: "white" }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return { ...styles, color: data.color };
+    },
+    multiValue: (styles, { data }) => {
+      return {
+        ...styles,
+        backgroundColor: data.color,
+        color: "#fff",
+      };
+    },
+    multiValueLabel: (styles, { data }) => {
+      return {
+        ...styles,
+        color: "#fff",
+      };
+    },
+    multiValueRemove: (styles, { data }) => {
+      return {
+        ...styles,
+        color: "#fff",
+        cursor: "pointer",
+        ":hover": {
+          color: "#fff",
+        },
+      };
+    },
+  };
+  const handleChange = (selectedOption, actionMeta) => {
+    console.log("handleChange", selectedOption, actionMeta);
+  };
+  const handleInputChange = (inputValue, actionMeta) => {
+    console.log("handleInputChange", inputValue, actionMeta);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React MultiSelect DropDown using React-select library.</h1>
+      <CreatableSelect
+        options={options}
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        isMulti
+        styles={colorStyles}
+      />
     </div>
+
   );
-}
+};
 
 export default App;
